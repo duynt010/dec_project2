25b7d5691e1ae8679dd8cb46ba009de83389d332
nguyenduy1001 <nguyenduy10011999@gmail.com>
2024-04-19 22:26:10 +0700

    fgh

--- models/Staging/stg_dim_person.sql
+++ models/Staging/stg_dim_person.sql
@@ -23,5 +23,33 @@ final AS (
 		person_suffix
 	FROM rename
 )
+	,dim_customer__add_undefined_record as (
+  SELECT 
+    person_key,
+	person_title,
+	person_first_name,
+	person_middle_name,
+	person_last_name,
+	person_suffix
+  FROM final
 
-SELECT * FROM final
+  UNION all
+  SELECT
+    0 as person_key,
+	'Undefined' as person_title,
+	'Undefined' as person_first_name,
+	'Undefined' as person_middle_name,
+	'Undefined' as person_last_name,
+	'Undefined' as person_suffix
+
+  UNION ALL
+  SELECT
+   -1 as person_key,
+	'Invalid' as person_title,
+	'Invalid' as person_first_name,
+	'Invalid' as person_middle_name,
+	'Invalid' as person_last_name,
+	'Invalid' as person_suffix
+  )
+
+SELECT * FROM dim_customer__add_undefined_record
