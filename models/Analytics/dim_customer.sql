2bf1380e780cb289004d6226d005155c2480ba01
nguyenduy1001 <nguyenduy10011999@gmail.com>
2024-04-19 23:38:36 +0700

    dim_customer

--- models/Analytics/dim_customer.sql
+++ models/Analytics/dim_customer.sql
@@ -1,16 +1,16 @@
 SELECT
-    dim_customer.customer_key
-    ,dim_customer.is_reseller
-    ,dim_customer.store_key as reseller_store_key
-    ,COALESCE(dim_store.store_name, 'Invalid') as reseller_store_name
-    ,dim_customer.person_key
-    ,dim_person.person_title
-    ,dim_person.person_first_name
-    ,dim_person.person_middle_name
-    ,dim_person.person_last_name
+  dim_customer.customer_key
+  ,dim_customer.is_reseller
+  ,dim_customer.store_key as reseller_store_key
+  ,COALESCE(dim_store.store_name, 'Invalid') as reseller_store_name
+  ,dim_customer.person_key
+  ,dim_person.person_title
+  ,dim_person.person_first_name
+  ,dim_person.person_middle_name
+  ,dim_person.person_last_name
 FROM
-    {{ref("stg_dim_customer")}} dim_customer
-    LEFT JOIN {{ref("stg_dim_person")}} dim_person
-        ON dim_customer.person_key = dim_person.person_key
-    LEFT JOIN {{ref("stg_dim_store")}} dim_store
-        on dim_store.store_key = dim_customer.store_key
+  {{ref("stg_dim_customer")}} dim_customer
+  LEFT JOIN {{ref("stg_dim_person")}} dim_person
+      ON dim_customer.person_key = dim_person.person_key
+  LEFT JOIN {{ref("stg_dim_store")}} dim_store
+      on dim_store.store_key = dim_customer.store_key
